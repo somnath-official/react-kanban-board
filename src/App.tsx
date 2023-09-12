0701f3e780123c8c0fbee009396767fa01820992
Somnath Official <somnath.sardar.aws@gmail.com>
2023-09-12 20:58:46 +0530

    Added kanban component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Kanban from './components/kanban/kanban'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Kanban />
     </>
   )
 }
---
+++ src/components/kanban/kanban.tsx
@@ -0,0 +1,7 @@
+const Kanban = () => {
+  return (
+    <h1>Kanban Board</h1>
+  )
+}
+
+export default Kanban
